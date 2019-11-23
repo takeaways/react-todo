1bb15f317b96385884ec0531072218c25191d598
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2019-11-23 22:29:27 +0900

    Updates

---
+++ precache-manifest.9efbeb017885a436ea73e68a841257b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18307b05ddfe2e61283ed722e79afc78",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "61a7d5576f1f791dea26",
+    "url": "/react-todo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4751b5838ce07252fdc5",
+    "url": "/react-todo/static/js/2.ae8d6792.chunk.js"
+  },
+  {
+    "revision": "61a7d5576f1f791dea26",
+    "url": "/react-todo/static/js/main.e77b647a.chunk.js"
+  },
+  {
+    "revision": "ec3b3ce7a5682a4537f6",
+    "url": "/react-todo/static/js/runtime-main.d3e3e13f.js"
+  }
+]);
